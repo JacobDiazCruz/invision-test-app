25c502640683baaca8da5ff7f5ac2ca5013f91cf
JacobDiazCruz <carljacobdiazcruz@gmail.com>
2023-08-25 15:54:35 +0800

    Remove: imageAlt key and replaced the image alt to title

--- src/app/components/LatestList.tsx
+++ src/app/components/LatestList.tsx
@@ -5,7 +5,6 @@ import FoodThree from "../../../public/assets/component-02/Image-03@2x.jpg";
 
 type ListItem = {
   image: StaticImageData;
-  imageAlt: string;
   title: string;
   description: string;
 };
@@ -14,19 +13,16 @@ export default function LatestList() {
   const listItems: ListItem[] = [
     {
       image: FoodOne,
-      imageAlt: "Summer Lunch By Mark Best",
       title: "Summer Lunch Menu By Mark Best",
       description: "AEG ambassador Mark Best's summer eats are guaranteed to help you make the most of the warmer weather and entertaining at home."
     },
     {
       image: FoodTwo,
-      imageAlt: "A Traditional Christmas Eve",
       title: "A Traditional Christmas Eve, Mark Best Style",
       description: "One of Australia's best chefs and AEG ambassador, Mark Best, shares his favourite Christmas Eve menu which is sure to impress your guests."
     },
     {
       image: FoodThree,
-      imageAlt: "Taking Taste Further",
       title: "Taking Taste Further",
       description: "This exclusive cookbook gives you all the know-how you need. We've designed it to make sure you get the most out of your products - and the best out of your food."
     }
@@ -43,7 +39,7 @@ export default function LatestList() {
             <div className="item">
               <Image
                 src={item.image}
-                alt={item.imageAlt}
+                alt={item.title}
               />
               <div className="content">
                 <h4 className="text-[18px] text-white mt-5">{item.title}</h4>
