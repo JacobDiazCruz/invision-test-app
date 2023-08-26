49722b7564e6f407b57e7b80cb1e0f52bacbeca5
JacobDiazCruz <carljacobdiazcruz@gmail.com>
2023-08-26 12:57:49 +0800

    Added: sectionOne config

--- src/config/contents.ts
+++ src/config/contents.ts
@@ -1,6 +1,29 @@
+import HoldingHands from "../../public/assets/component-01/Image-01@2x.jpg";
+import Veggies from "../../public/assets/component-01/Image-02@2x.jpg";
+import Veggie from "../../public/assets/component-01/Image-03@2x.jpg";
+
 import FoodOne from "../../public/assets/component-02/Image-01@2x.jpg";
 import FoodTwo from "../../public/assets/component-02/Image-02@2x.jpg";
 import FoodThree from "../../public/assets/component-02/Image-03@2x.jpg";
+
+export const sectionOne = {
+  title: "ANSWER YOUR BODY'S NEEDS",
+  images: [
+    HoldingHands,
+    Veggies,
+    Veggie
+  ],
+  body: [
+    {
+      subTitle: "",
+      paragraph: "The way ingredients are sourced affects the way we nourish our bodies. Author Mark Schatzer believes our body naturally develops an appetite for the foods and nutrients it needs to be healthy, but that artificial flavourings are getting in the way. This can be reversed by focusing on high-quality ingredients and being mindful as your appetite guides you to consume according to your body's needs.",
+    },
+    {
+      subTitle: "BE MINDFUL",
+      paragraph: "Sourcing local or organic food is a good way to start being more mindful about what you're cooking and eating.",
+    }
+  ]
+};
 
 export const sectionTwo = {
   title: "ALL THE LATEST FROM AEG",
@@ -12,7 +35,7 @@ export const sectionTwo = {
       button: {
         link: "/",
         value: "READ MORE"
-      }  
+      }
     },
     {
       image: FoodTwo,
